eff7a63e7f549eb393fd701437f2bb18e3d07658
clam <coderzww@gmail.com>
2018-02-12 23:54:41 +0800

    增加domparser

--- main.js
+++ main.js
@@ -7,22 +7,39 @@ function copyhtml2md(whereToPaste) {
     throw new Error(whereToPaste + " must be non-null.");
   }
 
-  function html2md(html) {}
+  function html2md(html) {
+    let mdStr = "";
+    let removeStyleHtml = html.replace(/style=\".*?\"/ig, "");
+
+    let parser = new DOMParser();
+    const doc = parser.parseFromString(removeStyleHtml, "text/html");
+    // thead
+    const thead = (doc.getElementsByTagName("thead") || [])[0];
+    if (thead) {
+      const theadTds = thead.getElementsByTagName('td');
+      (theadTds || []).reduce();
+    }
+
+    // tbody
+    let tbody = doc.getElementsByTagName("tbody");
+
+
+  }
+
   function pasteClipboardData(md) {}
 
-  ["paste", "cut"].forEach(eName => {
-    whereToPaste.addEventListener(
-      eName,
-      function(e) {
-        e = e || window.event;
-        var clipboardData = e.clipboardData || window.clipboardData;
-        if (clipboardData.types.indexOf("text/html") > -1) {
-          var html = clipboardData.getData("text/html");
+  whereToPaste.addEventListener(
+    "paste",
+    function (e) {
+      e = e || window.event;
+      let clipboardData = e.clipboardData || window.clipboardData;
+      if (clipboardData.types.indexOf("text/html") > -1) {
+        let html = clipboardData.getData("text/html");
+        let
           pasteClipboardData(html2md(html));
-          e.preventDefault();
-        }
-      },
-      false
-    );
-  });
+        e.preventDefault();
+      }
+    },
+    false
+  );
 }
